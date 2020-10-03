c7d0b778479753453eaf17dd6667d7f5170be6af
Patrick Mao <pmao@uw.edu>
2020-10-02 23:07:35 -0700

    revise UIBuilder, basic boxes

--- src/scenes/UIBuilder.hx
+++ src/scenes/UIBuilder.hx
@@ -1,21 +1,31 @@
 package scenes;
 
+import h2d.TileGroup;
 import h2d.Scene;
+import h2d.Object;
+import h2d.Bitmap;
 
-class UIBuilder implements Level{
+class UIBuilder{
 
-    public var scene: Scene;
+    private var UI: Object;
 
     public function new() {
-        this.scene = new Scene();
+        this.UI = new Object();
     }
 
-    public function init() {
-        
-    }
+    public function generateUI(): Object {
+        // should there be a separation of phases between player drafting the cards, and viewing the program being executed?
+        // maybe hide ui while actions are happening
 
-    public function update(dt: Float): Null<Level> {
-        return null;
+        var tempTile = h2d.Tile.fromColor(0xFF0000, 1920, 200);
+        var programBox = new Bitmap(tempTile, UI);
+        programBox.y = 880;
+
+        var tempTile2 = h2d.Tile.fromColor(0xFF0000, 400, 800);
+        var cardBox = new Bitmap(tempTile2, UI);
+        cardBox.x = 1520;
+
+        return UI;
     }
 
 }
